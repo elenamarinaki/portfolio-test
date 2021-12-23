cd9b48f69af6be464283912b69e8d85be0dcae01
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-23 03:06:42 +0000

    navbar additional styling - experimenting with tailwind

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,21 +4,30 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <header>
-      <div className='text-3xl text-white p-8'>
-        This is going to be a sample blog!
-      </div>
-      <div>
+      <div className='container mx-auto flex justify-between'>
         <nav className='flex flex-row justify-center items-center p-8 text-mustard space-x-4'>
-          <NavLink to='/' className='hover:underline'>
+          <NavLink
+            to='/'
+            className='hover:underline inline-flex items-center py-6 px-3 mr-4 text-2xl font-bold tracking-widest'
+          >
             Sanity
           </NavLink>
-          <NavLink to='/post' className='hover:underline'>
+          <NavLink
+            to='/post'
+            className='hover:underline inline-flex items-center py-6 px-3 mr-4 text-2xl font-bold tracking-widest'
+          >
             Blog
           </NavLink>
-          <NavLink to='/project' className='hover:underline'>
+          <NavLink
+            to='/project'
+            className='hover:underline inline-flex items-center py-6 px-3 mr-4 text-2xl font-bold tracking-widest'
+          >
             Projects
           </NavLink>
-          <NavLink to='/about' className='hover:underline'>
+          <NavLink
+            to='/about'
+            className='hover:underline inline-flex items-center py-6 px-3 mr-4 text-2xl font-bold tracking-widest'
+          >
             About
           </NavLink>
         </nav>

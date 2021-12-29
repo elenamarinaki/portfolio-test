8cda37e08de7b02cdc369bd27d49a9beb6b50e54
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2021-12-29 02:49:42 +0000

    layout the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,10 +21,14 @@ const About = () => {
   }, []);
 
   return (
-    <div className='font-mono h-screen bg-softBlack m-2'>
-      <p className='bg-red w-1/3 text-white bg-opacity-30 border-2 border-red rounded-lg p-4 hover:shadow-redGlow cursor-pointer'>
-        This is About page!
-      </p>
+    <div className='flex flex-col items-center text-white max-w-3xl sm:max-w-2xl mx-auto'>
+      {author ? (
+        <p className='bg-red w-1/3 text-white bg-opacity-30 border-2 border-red rounded-lg p-4 hover:shadow-redGlow cursor-pointer'>
+          This is About page!
+        </p>
+      ) : (
+        <div className='text-4xl mt-48'>Loading... 🍬</div>
+      )}
     </div>
   );
 };
